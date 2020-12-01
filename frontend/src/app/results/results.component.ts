78d8db20a7f5b1c6955e4aa4dd86c1f3a25d95bb
gabrielmcreynolds <gabrielmc785@gmail.com>
2020-11-30 20:35:56 -0600

    got results page done

---
+++ frontend/src/app/results/results.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { QuestionService } from '../question/question.service';
+import { Question } from '../question/question';
+
+@Component({
+  selector: 'app-results',
+  templateUrl: './results.component.html',
+  styleUrls: ['./results.component.scss'],
+})
+export class ResultsComponent implements OnInit {
+  public questions: Array<Question>;
+  public correctQuestions: number;
+
+  constructor(private questionService: QuestionService) {}
+
+  ngOnInit(): void {
+    this.questions = this.questionService.questions.value;
+  }
+
+  getReadableTime(): string {
+    const min = Math.floor(this.questionService.time / 60);
+    const sec =
+      this.questionService.time % 60 < 10
+        ? `0${this.questionService.time % 60}`
+        : `${this.questionService.time % 60}`;
+    return `${min}:${sec}`;
+  }
+}
