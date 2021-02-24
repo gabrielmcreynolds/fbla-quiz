d41ba6e1ec09b80466df32dc0585b9e575e3de1a
Gabriel McReynolds <49212895+gabrielmcreynolds@users.noreply.github.com>
2021-02-24 13:36:03 -0600

    added helper bot

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -4,9 +4,8 @@
 
 export const environment = {
   production: false,
-  // apiUrl: 'http://localhost:8080/'
-  apiUrl: 'https://backend-xqmdarcdtq-uc.a.run.app/'
-
+  apiUrl: 'http://localhost:8080/',
+  // apiUrl: 'https://backend-xqmdarcdtq-uc.a.run.app/'
 };
 
 /*
